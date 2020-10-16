f07e4aee384570bb1feb5ce69336aa04de6c73c3
joanaibarra <joana@expapp.com>
2020-10-15 22:34:51 -0400

    Init commit to master

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        KC Works - a construction company led by Kevin Cuevas 
         </p>
         <a
           className="App-link"
