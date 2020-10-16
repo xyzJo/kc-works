89c99aed0900b53945177fccb6ac2d56c1f44e7e
joanaibarra <joana@expapp.com>
2020-10-15 22:09:28 -0400

    Updates

---
+++ precache-manifest.740e2e4b5ab01d6c58b3008852a421a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27127a521af0bbe5af364152331503b8",
+    "url": "/kc-works/index.html"
+  },
+  {
+    "revision": "615b4c9a8250a0057974",
+    "url": "/kc-works/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7a062d7c6239253ee706",
+    "url": "/kc-works/static/js/2.6552ec91.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/kc-works/static/js/2.6552ec91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615b4c9a8250a0057974",
+    "url": "/kc-works/static/js/main.7515cf8d.chunk.js"
+  },
+  {
+    "revision": "73f0e30985cb51498aeb",
+    "url": "/kc-works/static/js/runtime-main.37c6d3b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kc-works/static/media/logo.5d5d9eef.svg"
+  }
+]);
